17fc2e27af1ab935afe64c034ea4d4c5947a9432
Samir Cəbrayıllı <samirjabrailli98@gmail.com>
2024-02-17 16:57:35 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function goster(){
+
+       
+    console.log("Salam");
+    console.log(1+3);
+    console.log("1"+3);
+    console.log("20" -"10");
+    let reqem1 = 5
+    let reqem2 = 15
+    console.log("cemi" + " " + (reqem1 + reqem2) );
+}
